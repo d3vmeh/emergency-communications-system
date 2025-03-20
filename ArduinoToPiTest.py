7af05404e439b57a598567da6c6ee897c6ee3942
Dev Mehra <devxmehra@gmail.com>
2025-03-19 22:46:13 -0700

    Update ArduinoToPiTest.py

--- ArduinoToPiTest.py
+++ ArduinoToPiTest.py
@@ -1,29 +1,20 @@
 import serial
 import time
 
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
-
-# Reset the Arduino's line. This is key to getting the write to work.
-# Without it, the first few writes don't work.
-# Clear DTR, wait one second, flush input, then set DTR.
-# Without this, the first write fails.
-# This trick was learned from:
-# https://github.com/miguelasd688/4-legged-robot-model
-
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 ser.setDTR(False)
 time.sleep(1)
 ser.reset_input_buffer()
 ser.setDTR(True)
 time.sleep(2)
-
-
 #IF THE SERIAL PORT WONT WORK, RESET THE ARDUINO WHILE IT IS CONNECTED TO THE PI
 while True:
     from_arduino = ser.readline().decode(errors='ignore').strip()  # Read full message, decode, and clean up
     if from_arduino:
         print(f"Arduino says: {from_arduino}")
-        print("Responding to Arduino")
-        ser.write(b"Hello Arduino\n")  # Send bytes with newline
+        #print("Responding to Arduino")
+        #ser.write(b"Hello Arduino\n")  # Send bytes with newline
         print("\n\n")
     #time.sleep(1)  # Avoid flooding
+
 
